02d6dc4bc39c721a90a12c604e69f9ff2812e042
Vakkuri, Elias <elias.vakkuri@cgi.com>
2021-05-08 20:43:20 +0300

    Add timestamp to logging

--- utils/middleware.js
+++ utils/middleware.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken')
 const logger = require('./logger')
 
 const requestLogger = (request, response, next) => {
+  logger.info('Timestamp:', new Date())
   logger.info('Method:', request.method)
   logger.info('Path:  ', request.path)
   logger.info('Body:  ', request.body)
